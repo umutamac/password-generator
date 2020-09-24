8fac09c19c24ea110282dfc4596eec1c6b27182e
Umut Alptekin <amacalptekin@gmail.com>
2020-09-24 18:21:28 +0300

    changed varaible names so that everything corresponds

--- script.js
+++ script.js
@@ -17,14 +17,14 @@ console.log("Include Numbers: " + numCheck);
 var uppercaseCheck = confirm("Would you like to include uppercase letters?");
 console.log("Include Uppercase Letters: " + uppercaseCheck);
 
-var passwordLength = prompt("Type in the length of the password you'd like (between 8 and 128)");
-console.log("Chosen password length: " + passwordLength);
-while (passwordLength < 8 || passwordLength > 128){
-  passwordLength = prompt("Please make sure to type in a number between 8 and 128");
-  console.log("Chosen password length: " + passwordLength);
+var chosenLength = prompt("Type in the length of the password you'd like (between 8 and 128)");
+console.log("Chosen password length: " + chosenLength);
+while (chosenLength < 8 || chosenLength > 128){
+  chosenLength = prompt("Please make sure to type in a number between 8 and 128");
+  console.log("Chosen password length: " + chosenLength);
 }
 
-//Create a char sets
+//Creating a char sets
 var charset1 = "abcdefghijklmnopqrstuvwxyz"
 var charset2 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var charset3 = "abcdefghijklmnopqrstuvwxyz0123456789"
@@ -34,34 +34,35 @@ var charset6 = "abcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+,-./:;<=>?@[]^_`{|
 var charset7 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
 var charset8 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
 
-// Choose a char set based on user answers above
+// Choosing a char set based on user answers above
 if (specialCharCheck == false && numCheck == false && uppercaseCheck == false){
-  charset = charset1;
+  var charset = charset1;
 } else if (specialCharCheck == false && numCheck == false && uppercaseCheck == true){
-  charset = charset2;
+  var charset = charset2;
 } else if (specialCharCheck == false && numCheck == true && uppercaseCheck == false){
-  charset = charset3;
+  var charset = charset3;
 } else if (specialCharCheck == true && numCheck == false && uppercaseCheck == false){
-  charset = charset4;
+  var charset = charset4;
 } else if (specialCharCheck == false && numCheck == true && uppercaseCheck == true){
-  charset = charset5;
+  var charset = charset5;
 } else if (specialCharCheck == true && numCheck == true && uppercaseCheck == false){
-  charset = charset6;
+  var charset = charset6;
 } else if (specialCharCheck == true && numCheck == false && uppercaseCheck == true){
-  charset = charset7;
+  var charset = charset7;
 } else {
-  charset = charset8;
+  var charset = charset8;
 }
 
 // Generate the password
-function generatePassword(){
+function generatePassword(chosenLength){
   
-var retVal = "";
-
-  for (var i = 0, n = charset.passwordLength; i < passwordLength; ++i) {
-    retVal += charset.charAt(Math.round(Math.random() * n));
-  }
+  var passwordText = "";
+    for (var i = 0, n = charset.length; i < chosenLength; ++i) {
+      passwordText += charset.charAt(Math.floor(Math.random() * n));
+    }
+  return passwordText;
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
