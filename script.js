f8c9074da4b982c0b5537137c75ca452ec8a311d
Umut Alptekin <amacalptekin@gmail.com>
2020-09-24 16:35:52 +0300

    added char sets for each possible case for user asnwers, set up if statements to pick a char set based on the answers, set up initial generator code

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function writePassword() {
   passwordText.value = password;
 }
 
+// Asking user if they want special chars, numbers, uppercase letters and how long of a password they want
 var specialCharCheck = confirm("Would you like to include special characters?");
 console.log("Include Special Characters: " + specialCharCheck);
 var numCheck = confirm("Would you like to include numbers?");
@@ -23,9 +24,43 @@ while (passwordLength < 8 || passwordLength > 128){
   console.log("Chosen password length: " + passwordLength);
 }
 
+//Create a char sets
+var charset1 = "abcdefghijklmnopqrstuvwxyz"
+var charset2 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var charset3 = "abcdefghijklmnopqrstuvwxyz0123456789"
+var charset4 = "abcdefghijklmnopqrstuvwxyz!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+var charset5 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+var charset6 = "abcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+var charset7 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+var charset8 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
 
-function generatePassword(specialCharCheck, numCheck, uppercaseCheck, passwordLength){
-  Math.random()*(passwordLength+1);
+// Choose a char set based on user answers above
+if (specialCharCheck == false && numCheck == false && uppercaseCheck == false){
+  charset = charset1;
+} else if (specialCharCheck == false && numCheck == false && uppercaseCheck == true){
+  charset = charset2;
+} else if (specialCharCheck == false && numCheck == true && uppercaseCheck == false){
+  charset = charset3;
+} else if (specialCharCheck == true && numCheck == false && uppercaseCheck == false){
+  charset = charset4;
+} else if (specialCharCheck == false && numCheck == true && uppercaseCheck == true){
+  charset = charset5;
+} else if (specialCharCheck == true && numCheck == true && uppercaseCheck == false){
+  charset = charset6;
+} else if (specialCharCheck == true && numCheck == false && uppercaseCheck == true){
+  charset = charset7;
+} else {
+  charset = charset8;
+}
+
+// Generate the password
+function generatePassword(){
+  
+var retVal = "";
+
+  for (var i = 0, n = charset.passwordLength; i < passwordLength; ++i) {
+    retVal += charset.charAt(Math.round(Math.random() * n));
+  }
 }
 
 // Add event listener to generate button
