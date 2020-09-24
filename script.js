62cd8cc11086a1372e76f500939353494433b7c9
Umut Alptekin <amacalptekin@gmail.com>
2020-09-24 15:10:06 +0300

    added confirm and prompt buttons to ask user if they want to include special chars, numbers, uppercase letter, and ask for a password length

--- script.js
+++ script.js
@@ -7,7 +7,25 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
+var specialCharCheck = confirm("Would you like to include special characters?");
+console.log("Include Special Characters: " + specialCharCheck);
+var numCheck = confirm("Would you like to include numbers?");
+console.log("Include Numbers: " + numCheck);
+var uppercaseCheck = confirm("Would you like to include uppercase letters?");
+console.log("Include Uppercase Letters: " + uppercaseCheck);
+
+var passwordLength = prompt("Type in the length of the password you'd like (between 8 and 128)");
+console.log("Chosen password length: " + passwordLength);
+while (passwordLength < 8 || passwordLength > 128){
+  passwordLength = prompt("Please make sure to type in a number between 8 and 128");
+  console.log("Chosen password length: " + passwordLength);
+}
+
+
+function generatePassword(specialCharCheck, numCheck, uppercaseCheck, passwordLength){
+  Math.random()*(passwordLength+1);
 }
 
 // Add event listener to generate button
